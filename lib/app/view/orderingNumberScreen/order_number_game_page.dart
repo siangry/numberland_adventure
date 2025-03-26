00e251098aa44e3dc4f9958c891d4e6363c62a0f
siangry <ruoying23@gmail.com>
2025-03-26 20:47:52 +0800

    feat: order number intro page

---
+++ lib/app/view/orderingNumberScreen/order_number_game_page.dart
@@ -0,0 +1,17 @@
+import 'package:numberland_adventure/app/exporter/importer_app_general.dart';
+
+class OrderNumberGamePage extends StatefulWidget {
+  final int levelId;
+
+  const OrderNumberGamePage({super.key, required this.levelId});
+
+  @override
+  State<OrderNumberGamePage> createState() => _OrderNumberGamePageState();
+}
+
+class _OrderNumberGamePageState extends State<OrderNumberGamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
